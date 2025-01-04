098a33c5e9128d1422f162a6c07323e5c06d7048
Charan <charan.reings125@gmail.com>
2025-01-04 10:32:46 +0530

    adding calendar

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,117 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import * as React from "react";
+import dayjs, { Dayjs } from "dayjs";
+import Badge from "@mui/material/Badge";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { PickersDay, PickersDayProps } from "@mui/x-date-pickers/PickersDay";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  const calendarRef = useRef<HTMLDivElement>(null);
+  const habitListRef = useRef<HTMLDivElement>(null);
+  const [calendarSize, setCalendarSize] = useState({ width: 0, height: 0 });
+  const [habitListSize, setHabitListSize] = useState({ width: 0, height: 0 });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useLayoutEffect(() => {
+    const resize = () => {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+    resize();
+
+    window.addEventListener("resize", resize);
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const calendarObserver = new ResizeObserver((entries) => {
+      console.log(entries);
+      for (let entry of entries) {
+        setCalendarSize({
+          width: entry.contentRect.width,
+          height: entry.contentRect.height,
+        });
+      }
+    });
+
+    const habitListObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        setHabitListSize({
+          width: entry.contentRect.width,
+          height: entry.contentRect.height,
+        });
+      }
+    });
+
+    if (calendarRef.current) {
+      calendarObserver.observe(calendarRef.current);
+    }
+
+    if (habitListRef.current) {
+      habitListObserver.observe(habitListRef.current);
+    }
+
+    return () => {
+      calendarObserver.disconnect();
+      habitListObserver.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="flex w-full min-h-screen gap-2 p-1 bg-slate-400 justify-evenly text-red-50">
+      <div
+        ref={calendarRef}
+        className="flex-1 min-w-[700px] p-3 bg-red-500 rounded ">
+        {`Window: w: ${size.width} h: ${size.height}`}
+        <br />
+        {`Calendar: w: ${calendarSize.width} h: ${calendarSize.height}`}
+        <DateCalendarServerRequest />
+      </div>
+      <div
+        ref={habitListRef}
+        className=" bg-red-500 min-w-[300px] p-3 w-[40vw] rounded">
+        {`Habit List: w: ${habitListSize.width} h: ${habitListSize.height}`}
+      </div>
     </div>
+  );
+}
+
+function DateCalendarServerRequest() {
+  const renderDays = (day, selectedDays, pickersDayProps) => {
+    return (
+      <Badge
+        key={day.toString()}
+        color="primary"
+        badgeContent={day == dayjs('day') ? "😶‍🌫️" : null}
+        overlap="circular">
+        <PickersDay {...pickersDayProps} />
+      </Badge>
+    );
+  };
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        displayWeekNumber
+        loading={false}
+        slots={{ day: renderDays }}
+        onChange={(value, state, view) => {
+          console.log(value.$M);
+          console.log(state);
+          console.log(view);
+        }}
+        renderLoading={() => <DayCalendarSkeleton />}
+      />
+    </LocalizationProvider>
   );
 }
