ffdfd044b20d5ad1c63de6f814d739e678c8d941
Charan <charan.reings125@gmail.com>
2025-01-08 22:38:19 +0530

    updated bnts logics

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -1,80 +1,31 @@
 "use client";
 
-import {
-  LocalizationProvider,
-  DateCalendar,
-  DayCalendarSkeleton,
-  PickersDayProps,
-  PickersDay,
-} from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { dateAtom } from "../atoms";
 import { useAtom } from "jotai";
-import dayjs, { Dayjs } from "dayjs";
-import { Badge } from "@mui/material";
-import { useRef } from "react";
+import { currentDateAtom } from "../atoms";
 
 export default function CustomDateCalendar() {
-  const [dateValue, setDateVale] = useAtom(dateAtom);
-  const highlightedDays = [1, 2, 15, 13];
-  const dateRef = useRef(null);
+  const [currentDateData, setCurrentDateData] = useAtom(currentDateAtom);
 
-  console.log("Date Ref: \n");
-  console.log(dateRef.current);
+  const totalDates = currentDateData.totalDays;
 
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar
-        className="m-0 !w-full md:!w-[50vw] rounded-lg md:static bg-[rgba(3,95,83,0.31)]  h-fit"
-        displayWeekNumber
-        showDaysOutsideCurrentMonth
-        loading={false}
-        value={dayjs(dateValue)}
-        ref={dateRef}
-        onChange={(value, state, view) => {
-          setDateVale(value);
-          console.log(value);
-          console.log(state);
-          console.log(view);
-        }}
-        renderLoading={() => <DayCalendarSkeleton />}
-        slots={{
-          day: ServerDay,
-        }}
-        slotProps={{
-          day: {
-            highlightedDays,
-          } as unknown as  PickersDayProps<Dayjs>,
-        }}
-      />
-    </LocalizationProvider>
-  );
-}
-
-function ServerDay(
-  props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }
-) {
-  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
-  // console.log("Props: \n" + )
-  console.log(props.day.date());
-  console.log("Highlighted Days: \n" + highlightedDays);
-  console.log(dayjs("2025-01-17").date());
-
-  const isSelected =
-    !props.outsideCurrentMonth &&
-    highlightedDays.indexOf(props.day.date()) >= 0 &&
-    dayjs("2025-01-15").date() === props.day.date();
+  const handelOnClick = (date: number) => {
+    setCurrentDateData((prev) => {
+      return { ...prev, date: date };
+    });
+  };
 
   return (
-    <Badge
-      key={props.day.toString()}
-      overlap="circular"
-      badgeContent={isSelected ? "🌚" : undefined}>
-      <PickersDay
-        {...other}
-        outsideCurrentMonth={outsideCurrentMonth}
-        day={day}
-      />
-    </Badge>
+    <div className="grid grid-cols-7 gap-1 p-2 border">
+      {Array.from({ length: totalDates }, (_, index) => {
+        return (
+          <div
+            onClick={() => handelOnClick(index + 1)}
+            key={index}
+            className="p-2 text-center transition-all duration-100 rounded bg-zinc-600 hover:bg-slate-400/70 hover:cursor-pointer ">
+            {index + 1}
+          </div>
+        );
+      })}
+    </div>
   );
 }
--- app/containers/calendar-container.tsx
+++ app/containers/calendar-container.tsx
@@ -1,19 +1,58 @@
 "use client";
 
-import { useAtomValue } from "jotai";
+import { useAtom } from "jotai";
 import CustomDateCalendar from "../components/calendar";
-import { dateAtom } from "../atoms";
 import dayjs from "dayjs";
+import { currentDateAtom } from "../atoms";
 
 export default function CalendarContainer() {
-  const dateValue = useAtomValue(dateAtom);
+  const [currentDateData, setCurrentDateData] = useAtom(currentDateAtom);
+
+  const nextBtn = () => {
+    setCurrentDateData((current) => {
+      const newMonth = current.month === 11 ? 0 : current.month + 1;
+      const newYear = current.month === 11 ? current.year + 1 : current.year;
+      return {
+        ...current,
+        month: newMonth,
+        year: newYear,
+        totalDays: dayjs(
+          `${newYear}-${newMonth + 1}-${current.date}`
+        ).daysInMonth(),
+      };
+    });
+  };
+
+  const prevBtn = () => {
+    setCurrentDateData((current) => {
+      const newMonth = current.month == 0 ? 11 : current.month - 1;
+      const newYear = current.month == 0 ? current.year - 1 : current.year;
+      return {
+        ...current,
+        month: newMonth,
+        year: newYear,
+        totalDays: dayjs(
+          `${newYear}-${newMonth + 1}-${current.date}`
+        ).daysInMonth(),
+      };
+    });
+  };
+
   return (
     <div className="flex-1 w-full h-full p-3 bg-red-300 rounded">
-      <CustomDateCalendar />
-
-      <div className="mt-5 rounded h-fit">
-        {dayjs(dateValue).format("DD-MM-YYYY")}
+      <div className="border flex justify-between py-1 px-5">
+        <button onClick={prevBtn}>Prev</button>
+        <span>
+          {dayjs(
+            `${currentDateData.year}-${currentDateData.month + 1}-${
+              currentDateData.date
+            }`
+          ).format("MMMM YYYY")}
+        </span>
+        <button onClick={nextBtn}>Next</button>
       </div>
+      <CustomDateCalendar />
+      {JSON.stringify(currentDateData, null, 2)}
     </div>
   );
 }
