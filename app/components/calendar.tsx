c869a2f10911821685db9d3c74a5f9f8cc6ebb46
Charan <charan.reings125@gmail.com>
2025-01-09 19:19:28 +0530

    custom bg for events

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -2,11 +2,14 @@
 
 import { useAtom } from "jotai";
 import { currentDateAtom } from "../atoms";
+import dayjs from "dayjs";
 
 export default function CustomDateCalendar() {
   const [currentDateData, setCurrentDateData] = useAtom(currentDateAtom);
 
   const totalDates = currentDateData.totalDays;
+  const events = [12, 3, 15, 31, 5, 10];
+  const colors = ["bg-red-500", "bg-yellow-500", "bg-orange-500", "bg-blue-500", "bg-white"];
 
   const handelOnClick = (date: number) => {
     setCurrentDateData((prev) => {
@@ -17,12 +20,20 @@ export default function CustomDateCalendar() {
   return (
     <div className="grid grid-cols-7 gap-1 p-2 border">
       {Array.from({ length: totalDates }, (_, index) => {
+        index = index + 1;
+        const today = index === currentDateData.date;
+        
+        const eventDay = events.includes(index) && currentDateData.month === dayjs().month() && dayjs().year() === currentDateData.year;
+        const eventColor = eventDay ? colors[index%colors.length] : "";
+
         return (
           <div
-            onClick={() => handelOnClick(index + 1)}
+            onClick={() => handelOnClick(index)}
             key={index}
-            className="p-2 text-center transition-all duration-100 rounded bg-zinc-600 hover:bg-slate-400/70 hover:cursor-pointer ">
-            {index + 1}
+            className={`p-2 text-center transition-all duration-100 rounded  hover:opacity-85 hover:cursor-pointer ${
+              eventDay ? `${eventColor}` : "bg-zinc-600"
+            } ${today ? "opacity-75" : ""}`}>
+            {index}
           </div>
         );
       })}
