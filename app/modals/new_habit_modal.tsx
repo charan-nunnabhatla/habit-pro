39a15cdad29c371e3c066665a715dc982d4e709f
Charan <charan.reings125@gmail.com>
2025-01-12 13:01:26 +0530

    style adjustments

--- app/modals/new_habit_modal.tsx
+++ app/modals/new_habit_modal.tsx
@@ -1,36 +1,11 @@
+import NewHabitModalForm from "../components/new-habit-modal-form";
+
 export default function NewHabitModal() {
   return (
     <dialog
       id="new-habit-dialog"
-      className="bg-slate-500 p-3 rounded top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] backdrop:backdrop-blur-sm h-full md:h-[50vh] overflow-auto w-full md:w-[50vw] transition-all duration-700 ">
-      <h1>New Habit</h1>
-      <form className="flex flex-col border" action="">
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          name="habit-name"
-          placeholder="Enter A Habit Name"
-          type="text"
-          required
-        />
-        <button type="reset">Reset</button>
-        <button
-          onClick={() => {
-            const dialog = document.getElementById(
-              "new-habit-dialog"
-            ) as HTMLDialogElement;
-            dialog.close();
-          }}>
-          Close
-        </button>
-        <select name="Fruits" id="fruits">
-          <option value="Apple">Apple</option>
-          <option value="Banana">Banana</option>
-          <option value="Kiwi">Kiwi</option>
-          <option value="Grape">Grape</option>
-          <option value="Orange">Orange</option>
-        </select>
-      </form>
+      className="bg-[rgb(124,134,133)]  border justify-center  p-3 rounded top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] backdrop:backdrop-blur-sm h-[clamp(40%,50vh,80vh)] overflow-auto w-full md:w-fit transition-all duration-700 ">
+        <NewHabitModalForm />
     </dialog>
   );
 }
