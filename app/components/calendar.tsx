6d8023dfd3048a43784f42131aa10866138f5626
Charan <charan.reings125@gmail.com>
2025-01-12 18:29:41 +0530

    style adjustments

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -1,20 +1,12 @@
 "use client";
 
 import { useAtom } from "jotai";
-import { currentDateAtom } from "../atoms";
-import dayjs from "dayjs";
+import { customDateAtom } from "../atoms";
 
 export default function CustomDateCalendar() {
-  const [currentDateData, setCurrentDateData] = useAtom(currentDateAtom);
+  const [currentDate, setCurrentDateData] = useAtom(customDateAtom);
 
-  const totalDates = currentDateData.totalDays;
-  const events = [12, 3, 15, 31, 5];
-  const colors = [
-    "bg-red-500",
-    "bg-yellow-500",
-    "bg-orange-500",
-    "bg-blue-500",
-  ];
+  const totalDates = currentDate.totalDays;
 
   const handelOnClick = (date: number) => {
     setCurrentDateData((prev) => {
@@ -23,24 +15,17 @@ export default function CustomDateCalendar() {
   };
 
   return (
-    <div className="grid grid-cols-7 gap-1 p-2 border">
+    <div className="grid grid-cols-7 gap-1 p-2 rounded outline outline-lime-200 outline-2">
       {Array.from({ length: totalDates }, (_, index) => {
         index = index + 1;
-        const today = index === currentDateData.date;
-
-        const eventDay =
-          events.includes(index) &&
-          currentDateData.month === dayjs().month() &&
-          dayjs().year() === currentDateData.year;
-        const eventColor = eventDay ? colors[index % colors.length] : "";
+        const today = index === currentDate.date;
 
         return (
           <div
             onClick={() => handelOnClick(index)}
             key={index}
-            className={`p-2 text-center transition-all duration-100 rounded  hover:opacity-85 hover:cursor-pointer ${
-              eventDay ? `${eventColor}` : "bg-zinc-600"
-            } ${today ? "outline-2 outline outline-emerald-500" : ""}`}>
+            className={`p-2 text-center transition-all duration-100 rounded  hover:opacity-85 hover:cursor-pointer bg-zinc-600
+             ${today ? "outline-2 outline outline-emerald-500" : ""}`}>
             <span className="font-bold text-white opacity-100 ">{index}</span>
           </div>
         );
