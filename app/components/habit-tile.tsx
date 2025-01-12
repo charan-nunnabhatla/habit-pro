ddd9828a7aa7cb184e1c93c991bd171b4d76f4e4
Charan <charan.reings125@gmail.com>
2025-01-12 18:30:37 +0530

    style adjustments

--- app/components/habit-tile.tsx
+++ app/components/habit-tile.tsx
@@ -3,15 +3,20 @@ import { habitDataAtom, habitType } from "../atoms";
 import dayjs from "dayjs";
 import markHabitComplete from "../operations/completed-habit";
 import deleteHabit from "../operations/delete-habit";
+import { MdDeleteForever } from "react-icons/md";
 
 export default function HabitTile(props: habitType) {
   const setHabitData = useSetAtom(habitDataAtom);
   return (
-    <div className="my-2 border group">
+    <div
+      onClick={() => markHabitComplete(setHabitData, props.id)}
+      className={`mb-3 cursor-pointer outline bg-[rgba(255,255,255,0.15)] hover:bg-[rgba(255,255,255,0.2)] ${
+        !props.completed ? "outline-lime-400" : "outline-red-600"
+      } rounded group`}>
       <div
         className={`flex flex-row w-full px-3 py-2 ${
           props.completed ? "line-through" : ""
-        } border rounded`}>
+        } rounded`}>
         <div className="mr-auto">{props.title}</div>
         <div className="px-3">{props.time}</div>
         <div className="">
@@ -24,16 +29,17 @@ export default function HabitTile(props: habitType) {
           )}
         </div>
       </div>
-      <div className="w-full p-1 border border-emerald-950">
-        <button
+      <div className="w-full p-1 border-emerald-950">
+        {/* <button
           onClick={() => markHabitComplete(setHabitData, props.id)}
           className="p-1 text-xs border rounded ">
           Done
-        </button>
+        </button> */}
         <button
-          className="p-1 text-xs border rounded"
+          title="Delete"
+          className="p-1 text-xs"
           onClick={() => deleteHabit(setHabitData, props.id)}>
-          Delete
+          <MdDeleteForever className="text-white text-[1rem] font-bold" />
         </button>
       </div>
     </div>
--- app/components/modal-action-buttons.tsx
+++ app/components/modal-action-buttons.tsx
@@ -1,16 +1,23 @@
+import { useSetAtom } from "jotai";
+import { addHabit } from "../operations/add-habit";
+import { habitDataAtom } from "../atoms";
 import closeModal from "../operations/modal-close-button";
 
 export default function ModalActionButtons() {
+  const setHabitData = useSetAtom(habitDataAtom);
   return (
     <div
       id="action-buttons"
-      className="flex absolute w-full right-0 bottom-0 flex-row items-center justify-center gap-3 p-3 ml-auto border ">
-      <button type="reset">Reset</button>
-      <button className="mr-auto" onClick={closeModal}>
-        Close
-      </button>
-      <button className="" onClick={closeModal}>
+      className="flex absolute w-full right-0 bottom-0 flex-row items-center justify-center gap-3 p-3 ml-auto ">
+      <button
+        className=" w-full py-2 px-3 rounded bg-emerald-400 font-bold text-white text-xl hover:bg-emerald-500 transition-all duration-150"
+        onClick={() => addHabit(setHabitData)}>
         Done
+      </button>
+      <button
+        className=" w-full py-2 px-3 rounded bg-red-400 font-bold text-white text-xl hover:bg-red-500 transition-all duration-150"
+        onClick={closeModal}>
+        Close
       </button>
     </div>
   );
