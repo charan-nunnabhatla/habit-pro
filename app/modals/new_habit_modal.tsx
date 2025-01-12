2197e63dc7747301df5a391ec2a31b72fb8893cd
Charan <charan.reings125@gmail.com>
2025-01-12 18:32:00 +0530

    style adjustments

--- app/modals/new_habit_modal.tsx
+++ app/modals/new_habit_modal.tsx
@@ -4,8 +4,8 @@ export default function NewHabitModal() {
   return (
     <dialog
       id="new-habit-dialog"
-      className="bg-[rgb(124,134,133)]  border justify-center  p-3 rounded top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] backdrop:backdrop-blur-sm h-[clamp(40%,50vh,80vh)] overflow-auto w-full md:w-fit transition-all duration-700 ">
-        <NewHabitModalForm />
+      className="bg-zinc-800 justify-center  p-3 rounded top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] backdrop:backdrop-blur-sm h-full md:h-[clamp(40%,50vh,80vh)] overflow-auto w-full md:w-fit transition-all duration-700 ">
+      <NewHabitModalForm />
     </dialog>
   );
 }
