2e8fa5f2bed141e70418ab375b5abc5b6add7abb
Charan <charan.reings125@gmail.com>
2025-01-14 11:10:45 +0530

    added funtionality to action buttons

--- app/components/modal-action-buttons.tsx
+++ app/components/modal-action-buttons.tsx
@@ -3,21 +3,29 @@ import { addHabit } from "../operations/add-habit";
 import { habitDataAtom } from "../atoms";
 import closeModal from "../operations/modal-close-button";
 
-export default function ModalActionButtons() {
+export default function ModalActionButtons({
+  closeModalID,
+  root,
+  closeName,
+  addName,
+}: {
+  closeModalID: string;
+  root: boolean;
+  closeName: string;
+  addName: string;
+}) {
   const setHabitData = useSetAtom(habitDataAtom);
   return (
-    <div
-      id="action-buttons"
-      className="flex absolute w-full right-0 bottom-0 flex-row items-center justify-center gap-3 p-3 ml-auto ">
+    <div id="action-buttons" className="flex w-full gap-3 p-3 ml-auto ">
       <button
-        className=" w-full py-2 px-3 rounded bg-emerald-400 font-bold text-white text-xl hover:bg-emerald-500 transition-all duration-150"
-        onClick={() => addHabit(setHabitData)}>
-        Done
+        className="w-full px-3 py-2 text-xl font-bold text-white transition-all duration-150 rounded bg-emerald-400 hover:bg-emerald-500"
+        onClick={() => addHabit(setHabitData, { root })}>
+        {addName}
       </button>
       <button
-        className=" w-full py-2 px-3 rounded bg-red-400 font-bold text-white text-xl hover:bg-red-500 transition-all duration-150"
-        onClick={closeModal}>
-        Close
+        className="w-full px-3 py-2 text-xl font-bold text-white transition-all duration-150 bg-red-400 rounded hover:bg-red-500"
+        onClick={() => closeModal(closeModalID)}>
+        {closeName}
       </button>
     </div>
   );
