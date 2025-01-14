bbd2e9c1f60384a510b0c7bd891ea1706acca0af
Charan <charan.reings125@gmail.com>
2025-01-14 11:17:01 +0530

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import HabitsListContainer from "./containers/habits-list-container";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen gap-2 p-2 bg-black rounded md:flex-row">
+    <div
+      id="root"
+      className="flex flex-col items-center justify-center w-full h-screen gap-2 p-2 bg-black rounded md:flex-row">
       <CalendarContainer />
       <HabitsListContainer />
     </div>
