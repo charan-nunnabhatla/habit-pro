48529094da20fe4b2e7375b5a65e8e657565c1ae
Charan <charan.reings125@gmail.com>
2025-01-14 11:09:43 +0530

    removed defaultvalues

--- app/components/date-time-picker.tsx
+++ app/components/date-time-picker.tsx
@@ -1,21 +1,19 @@
-import dayjs from "dayjs";
-
 export default function DateTimePickers() {
   return (
-    <div className="flex flex-row border m-3">
+    <div className="flex flex-row m-3">
       <input
         id="time-input"
-        defaultValue={dayjs().format("HH:MM")}
+        // defaultValue={dayjs().format("HH:mm")}
         className="p-3 m-3 font-bold text-gray-500 rounded outline-none focus:ring focus:ring-blue-500 w-fit"
         type="time"
         title="Select a time"
       />
       <input
         id="date-input"
-        defaultValue={dayjs().format("YYYY-MM-DD")}
+        // defaultValue={dayjs().format("YYYY-MM-DD")}
         className="p-3 m-3 font-bold text-gray-500 rounded outline-none focus:ring focus:ring-blue-500 w-fit"
         type="date"
-        title="Select a date"
+        title="MM/DD/YYYY"
       />
     </div>
   );
