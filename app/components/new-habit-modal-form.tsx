e1f0c8f52b29a69e9041edc10808dbc263985223
Charan <charan.reings125@gmail.com>
2025-01-14 11:11:15 +0530

    style adjust

--- app/components/new-habit-modal-form.tsx
+++ app/components/new-habit-modal-form.tsx
@@ -7,20 +7,27 @@ export default function NewHabitModalForm() {
     // ! dont use <form> tag as it appends the data 2 times
     // * event close triggers the onSubmit action
     <div className="relative flex flex-col items-center justify-start flex-grow h-full p-2 ">
-      <span className="text-white p-3 font-bold">Add New Task</span>
+      <span className="p-3 font-bold text-white">Add New Task</span>
       <div className="flex flex-col items-start w-full gap-2">
-        <label className="text-white font-bold text-lg" htmlFor="title-input">
+        <label className="text-lg font-bold text-white" htmlFor="title-input">
           Name
         </label>
         <input
           id="title-input"
-          className="p-1 w-full font-bold text-black placeholder:text-gray-500 capitalize bg-transparent bg-white rounded outline-none"
+          className="w-full p-1 font-bold text-gray-600 capitalize bg-transparent bg-white rounded outline-none placeholder:text-gray-500"
           placeholder="Enter A Habit Name"
           type="text"
         />
       </div>
       <DateTimePickers />
-      <ModalActionButtons />
+      <div className="absolute bottom-0 w-full">
+        <ModalActionButtons
+          closeModalID="new-habit-dialog"
+          root={true}
+          addName="Add"
+          closeName="Close"
+        />
+      </div>
     </div>
   );
 }
