a81f4e0511e523535373edb3e5068e59ebac8721
Charan <charan.reings125@gmail.com>
2025-01-18 15:15:26 +0530

    added bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  // Other Next.js configurations
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
