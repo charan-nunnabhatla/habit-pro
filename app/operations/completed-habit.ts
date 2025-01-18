dc6d6ae0a29a4a92520a4315f5156c077afbbd6c
Charan <charan.reings125@gmail.com>
2025-01-18 09:44:30 +0530

    checks click event element

--- app/operations/completed-habit.ts
+++ app/operations/completed-habit.ts
@@ -1,14 +1,17 @@
+import { MouseEventHandler } from "react";
 import { habitType } from "../atoms";
 
 export default function markHabitComplete(
   setHabitData: React.Dispatch<React.SetStateAction<habitType[]>>,
-  id: string
+  id: string,
+  event?: React.MouseEvent<HTMLDivElement>
 ) {
-  setHabitData((prev) =>
-    prev.map((habit) => {
-      return habit.id === id
-        ? { ...habit, completed: !habit.completed }
-        : habit;
-    })
-  );
+  if (event?.target instanceof HTMLDivElement || event === undefined)
+    setHabitData((prev) =>
+      prev.map((habit) => {
+        return habit.id === id
+          ? { ...habit, completed: !habit.completed }
+          : habit;
+      })
+    );
 }
