9fbbaa1b59d2c91d948fa854344e1c9148f8e044
Charan <charan.reings125@gmail.com>
2025-01-18 09:39:49 +0530

    can rename title

--- app/containers/habits-list-container.tsx
+++ app/containers/habits-list-container.tsx
@@ -9,20 +9,30 @@ import { habitDataAtom } from "../atoms";
 export default function HabitsListContainer() {
   const habitData = useAtomValue(habitDataAtom);
   return (
-    <div className="relative h-full min-w-max w-full p-3 bg-zinc-800 rounded size-5 duration-1000  md:w-[60vw] md:h-full overflow-auto">
+    <div className="flex flex-col scroll-bar relative h-full w-full lg:w-max p-3 bg-zinc-800 rounded duration-1000  md:w-auto md:h-full overflow-auto">
       <NewHabitModal />
-      {Array.from(habitData).map((value) => {
-        return (
-          <HabitTile
-            id={value.id}
-            key={value.id}
-            time={value.time}
-            date={value.date}
-            title={value.title}
-            completed={value.completed}
-          />
-        );
-      })}
+      {habitData.length > 0 ? (
+        <div className="">
+          {Array.from(habitData).map((value) => {
+            return (
+              <HabitTile
+                id={value.id}
+                key={value.id}
+                time={value.time}
+                date={value.date}
+                title={value.title}
+                completed={value.completed}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-full w-full text-white font-bold">
+          No Tasks!
+          <br />
+          Click on the Add button to add a task
+        </div>
+      )}
       <AddNewHabitButton />
     </div>
   );
