dfc3d12a31e9d65eeb55a22865c67a232c74b52f
Charan <charan.reings125@gmail.com>
2025-02-07 11:42:20 +0530

    using semantic tags

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,26 @@ import NotesContainer from "./containers/notes-container";
 
 export default function Home() {
   return (
-    <div className="min-h-svh w-screen lg:w-[60vw] mx-auto">
+    <main className="min-h-svh w-screen lg:w-[60vw] mx-auto">
       <div
         id="root"
         className=" grid grid-cols-[1fr] grid-rows-[auto,1fr,1fr] md:grid-rows-[auto,1fr] md:grid-cols-[1fr,fit-content] w-full min-h-svh gap-1 justify-center p-1 bg-black rounded">
-        <div
+        <section
           id="calendar"
           className="row-start-1 md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2">
           <CalendarContainer />
-        </div>
-        <div
+        </section>
+        <section
           id="habitslist"
           className="row-start-2 md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3">
           <HabitsListContainer />
-        </div>
-        <div
+        </section>
+        <section
           id="notes"
           className="row-start-3 md:row-start-2 md:col-start-1 md:col-end-2  ">
           <NotesContainer />
-        </div>
+        </section>
       </div>
-    </div>
+    </main>
   );
 }
